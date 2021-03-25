6c2f7b28d9728cd0ebd7ebc242bb6f48fd81a470
kstick9210 <kstick9210@outlook.com>
2021-03-25 15:16:54 -0500

    create folder structure and reusable screen component

---
+++ app/screens/MarsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function MarsScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>El Planeta Rojo</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
