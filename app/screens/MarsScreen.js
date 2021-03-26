88b4d77ab41bddadff4c0211c49fa79bbef5ee53
kstick9210 <kstick9210@outlook.com>
2021-03-26 11:37:59 -0500

    fix scroll issue

--- app/screens/MarsScreen.js
+++ app/screens/MarsScreen.js
@@ -7,44 +7,52 @@ const { earthVmars: { earth, mars } } = marsInfo
 
 export default function MarsScreen() {
     return (
-        <ScrollView contentContainerStyle={styles.scroll}>
-            <ImageBackground 
-                resizeMode="cover"
-                source={require("../assets/mars-insight.jpeg")}
-                style={styles.background}
+        <View style={styles.view}>
+            <ScrollView 
+                fadingEdgeLength={2}
             >
-                <View style={styles.view}>
-                    <Text style={styles.header}>El Planeta Rojo</Text>
+                <ImageBackground 
+                    resizeMode="cover"
+                    source={require("../assets/mars-insight.jpeg")}
+                    style={styles.background}
+                >
+                    <View style={styles.view}>
+                        <Text style={styles.header}>El Planeta Rojo</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.bottom}>
+                    <Text style={styles.text}>
+                        {marsInfo.summary}
+                    </Text>
+                    <Text style={styles.title}>
+                        Moons
+                    </Text>
+                    <Text style={styles.text}>
+                        {marsInfo.moons[0]}  {marsInfo.moons[1]}
+                    </Text>
+                    <Text style={styles.title}>
+                        Earth vs Mars
+                    </Text>
+                    <Text style={styles.text}>
+                        Average Temp{'\n'}
+                        {earth.avgTemp[0]}  {mars.avgTemp[0]}{'\n'}
+                        {earth.avgTemp[1]}  {mars.avgTemp[1]}{'\n'}{'\n'}
+                        Earth Day  Mars Sol{'\n'}
+                        {earth.day}   {mars.sol}{'\n'}{'\n'}
+                        Year{'\n'}
+                        {earth.year[0]}  {mars.year[0]}{'\n'}
+                        {earth.year[1]}    {mars.year[1]}{'\n'}{'\n'}
+                        Tallest Mountain{'\n'}
+                        {earth.tallestMtn.name} {mars.tallestMtn.name}{'\n'}
+                        {earth.tallestMtn.height} {mars.tallestMtn.height}{'\n'}{'\n'}
+                        Deepest Canyon{'\n'}
+                        {earth.deepestCanyon.name} {mars.deepestCanyon.name}{'\n'}
+                        {earth.deepestCanyon.depth} {mars.deepestCanyon.depth}{'\n'}{'\n'}
+                        Diameter{'\n'}
+                    </Text>
                 </View>
-            </ImageBackground>
-            <View style={styles.bottom}>
-                <Text style={styles.text}>
-                    {marsInfo.summary}
-                </Text>
-                <Text style={styles.title}>
-                    Moons
-                </Text>
-                <Text style={styles.text}>
-                    {marsInfo.moons[0]}  {marsInfo.moons[1]}
-                </Text>
-                <Text style={styles.title}>
-                    Earth vs Mars
-                </Text>
-                <Text style={styles.text}>
-                    Average Temp{'\n'}
-                    {earth.avgTemp[0]}  {mars.avgTemp[0]}{'\n'}
-                    {earth.avgTemp[1]}  {mars.avgTemp[1]}{'\n'}{'\n'}
-                    Earth Day  Mars Sol{'\n'}
-                    {earth.day}   {mars.sol}{'\n'}{'\n'}
-                    Year{'\n'}
-                    {earth.year[0]}  {mars.year[0]}{'\n'}
-                    {earth.year[1]}    {mars.year[1]}{'\n'}{'\n'}
-                    Tallest Mountain{'\n'}
-                    Deepest Canyon{'\n'}
-                    Diameter{'\n'}
-                </Text>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </View>
     )
 }
 
@@ -52,11 +60,11 @@ const styles = StyleSheet.create({
     background: {
         alignItems: 'center',
         justifyContent: 'center',
-        resizeMode: "repeat",
-        flex: 1,
+        // height: 20
+        // flex: 2
     },
     bottom: {
-        flex: 3,
+        // flex: 3,
         // backgroundColor: 'rgb(153, 97, 65)',
         backgroundColor: 'brown',
         alignItems: 'center'
@@ -65,9 +73,6 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 30,
         marginTop: 45
-    },
-    scroll: {
-        flexGrow: 1
     },
     title: {
         color: 'white',
